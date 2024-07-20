00ecade9c86294eba91c6eddc50b3e6d18b7143f
Julian Prud'homme <julian.prudhomme@akeneo.com>
2024-07-20 12:31:03 +0200

    Add a menu to change the theme and restart the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {ThemeProvider, createTheme} from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -7,12 +7,33 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import Game from './components/Game';
 import {GameProvider} from "./contexts/gameContext";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from '@mui/material/Menu';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import MenuIcon from '@mui/icons-material/Menu';
+import ReplayIcon from '@mui/icons-material/Replay';
 
-type PaletteMode = 'light' | 'dark';
+enum PaletteMode {
+    light = 'light',
+    dark = 'dark'
+}
 
 export default function App() {
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-    const [colorMode, setColorMode] = React.useState<PaletteMode>(prefersDarkMode ? 'dark' : 'light');
+    const colorModeChoice: string | null = localStorage.getItem('colorMode');
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const [colorMode, setColorMode] = useState<PaletteMode>(
+        colorModeChoice !== null && Object.values(PaletteMode).includes(colorModeChoice as PaletteMode)
+        ? colorModeChoice as PaletteMode
+        : prefersDarkMode ? PaletteMode.dark : PaletteMode.light
+    );
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     const theme = React.useMemo(
         () => createTheme({palette: {mode: colorMode}}),
@@ -20,15 +41,43 @@ export default function App() {
     );
 
     const toggleColorMode = () => {
-        setColorMode((previousMode) => (previousMode === 'light' ? 'dark' : 'light'));
+        const newMode = colorMode === PaletteMode.light ? PaletteMode.dark : PaletteMode.light;
+        setColorMode(newMode);
+        localStorage.setItem('colorMode', newMode);
+    }
+
+    const restartGame = () => {
+        localStorage.removeItem('gameState');
+        window.location.reload();
     }
 
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline/>
-            <IconButton sx={{position: "fixed", bottom: "0px", left: "0px"}} onClick={toggleColorMode} color="inherit">
-                {theme.palette.mode === 'dark' ? <LightModeIcon/> : <DarkModeIcon/>}
+            <IconButton sx={{position: "fixed", top: "12px", left: "0px"}} onClick={handleClick} size="small">
+                <MenuIcon/>
             </IconButton>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem onClick={() => {handleClose(); toggleColorMode()}}>
+                    <ListItemIcon>
+                        {theme.palette.mode === 'dark' ? <LightModeIcon/> : <DarkModeIcon/>}
+                    </ListItemIcon>
+                    Thème {theme.palette.mode === 'dark' ? "clair" : "sombre"}
+                </MenuItem>
+                <MenuItem onClick={() => {handleClose(); restartGame()}}>
+                    <ListItemIcon>
+                        <ReplayIcon/>
+                    </ListItemIcon>
+                    Recommencer
+                </MenuItem>
+            </Menu>
             <GameProvider>
                 <Game/>
             </GameProvider>
